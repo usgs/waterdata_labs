746491adaa4304df167635e18302dbc5944dc8dd
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-05-06 15:53:45 -0500

    testing aws deploy

--- awsTierDeploy.sh
+++ awsTierDeploy.sh
@@ -4,6 +4,6 @@ URL=$1
 
 #aws s3 sync /src/public s3://${URL} --delete
 
-echo "testing" > /scr/testing.txt
+echo "testing" > testing.txt
 
-#aws s3 sync /scr/testing.txt s3://${URL} --delete
+aws s3 sync testing.txt s3://${URL} --delete
