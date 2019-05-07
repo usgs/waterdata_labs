acd8fe0fe8ec21fe27697e7a173ff7a126af989f
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-05-07 08:17:57 -0500

    testing returned command to deploy to aws

--- awsTierDeploy.sh
+++ awsTierDeploy.sh
@@ -2,8 +2,4 @@
 
 URL=$1
 
-#aws s3 sync /src/public s3://${URL} --delete
-
-echo "testing" > testing.txt
-
-aws s3 sync testing.txt s3://${URL} --delete
+aws s3 sync /src/public s3://${URL} --delete
