45bc3d090211ac748c3d0dc3cb395e194749aa8b
mbucknell <mbucknell@usgs.gov>
2020-04-15 10:43:49 -0500

    Updated dependencies and removed unused dependencies and updated the Dockerfile to use python 3.8 and the latest version of Hugo 0.69.0

--- themes/wdfn_theme/rollup.config.js
+++ themes/wdfn_theme/rollup.config.js
@@ -5,7 +5,7 @@
 const path = require('path');
 
 const alias = require('@rollup/plugin-alias');
-const buble = require('rollup-plugin-buble');
+const buble = require('@rollup/plugin-buble');
 const resolve = require('rollup-plugin-node-resolve');
 const { uglify } = require('rollup-plugin-uglify');
 
