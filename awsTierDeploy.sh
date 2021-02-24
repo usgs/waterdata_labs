8bb08bbd9c5deb45d083f36b9e6cee9432998c8c
Aaron Briggs <abriggs@contractor.usgs.gov>
2021-02-24 15:14:09 -0600

    change to aws deploy

--- awsTierDeploy.sh
+++ awsTierDeploy.sh
@@ -2,7 +2,7 @@
 
 URL=$1
 
-aws s3 rm s3://${URL} --recursive
+aws s3 rm s3://${URL} --recursive --exclude "test-lookups/*"
 echo "Cleaned bucket prior to deployment."
-aws s3 sync /src/public s3://${URL} --delete
+aws s3 sync /src/public s3://${URL}
 echo "Ran AWS sync command."
